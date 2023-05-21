abe5b9221a80a6123feb1510183b0b5486c6ee4f
unknown <turjoyplaban09@gmail.com>
2023-05-21 22:03:14 +0600

    Project is almost done

--- HomeZone_Main.cpp
+++ HomeZone_Main.cpp
@@ -5,15 +5,683 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "Login.cpp"
-
-
-
-
 
 using namespace std;
+bool state_of_cart=true;
+string Name_customer = "";
+
+
+
+class Buying_Land;
+
+
+class Card_view_Adding
+{
+    char line[600];
+    int N = 800;
+public:
+    void added_to_cart(int flag,string what_file)
+    {
+        int counter=0,c_ter=0;
+        ifstream fout;
+        fout.open(what_file);
+
+        if(flag >= 22)
+        {
+            counter = flag + 11;
+        }
+        else if(flag >=19)
+        {
+            counter = flag + 10;
+        }
+        else if(flag >=16)
+        {
+            counter = flag + 9;
+        }
+        else if(flag >=13)
+        {
+            counter = flag + 8;
+        }
+        else if(flag >=10)
+        {
+            counter = flag + 7;
+        }
+        else if(flag >=7)
+        {
+            counter = flag + 6;
+        }
+        else if(flag >=4)
+        {
+            counter = flag + 5;
+        }
+        else if(flag >=1)
+        {
+            counter = flag + 4;
+        }
+
+
+        if(flag != 0)
+        {
+            while (fout)
+            {
+                c_ter++;
+                fout.getline(line, N);
+                if(c_ter==counter)
+                {
+                    if(state_of_cart)
+                    {
+                        state_of_cart = false;
+                        ofstream card_view("card_view.txt",ios::out);
+                        card_view<<line<<endl;
+                    }
+                    else
+                    {
+                        ofstream card_view("card_view.txt",ios::app);
+                        card_view<<line<<endl;
+                    }
+
+                    system("cls");
+
+                    cout << line << endl;
+                    cout << endl << "Item is added to Cart Successfully" << endl;
+                }
+            }
+        }
+        else
+        {
+            while (fout)
+            {
+                fout.getline(line, N);
+                cout << line << endl;
+            }
+        }
+
+
+
+
+        if(flag != 0)
+        {
+
+            cout << endl << "Do you want to stay more time ?? (1 for YES, 0 for NO)" <<endl;
+            cout << "Give input :: " ;
+            int a;
+            cin >> a;
+            if(a==1)
+            {
+                system("cls");
+                added_to_cart(0,what_file);
+            }
+            else if(a==0)
+            {
+                return;
+            }
+            else
+            {
+                cout << "Wrong Input " << endl;
+                return;
+            }
+
+        }
+        else
+        {
+            int city_pos;
+            cout << "Choose City :: ";
+            cin >> city_pos;
+            added_to_cart(city_pos,what_file);
+
+        }
+    }
+};
+
+
+class DashBoard
+{
+
+    char line[600];
+    int N = 800;
+public:
+
+    virtual void choose_city(int flag) = 0; // Pure virtual function used
+
+    void choose_country()
+    {
+        ifstream fin_country;
+        fin_country.open("country.txt");
+        while (fin_country)
+        {
+            fin_country.getline(line, N);
+            cout << line << endl;
+        }
+        cout << "Choose Country :: ";
+        int flag;
+        cin >> flag;
+        switch(flag)
+        {
+        case 1:
+            cout << "**********************"<<"Bangladesh"<< "**********************" << endl;
+            choose_city(flag);
+            break;
+        case 2:
+            cout << "**********************"<<"USA"<< "**********************" << endl;
+            choose_city(flag);
+            break;
+        case 3:
+            cout << "**********************"<<"Canada"<< "**********************" << endl;
+            choose_city(flag);
+            break;
+        }
+
+    }
+};
+
+
+class Buying_Land : public DashBoard // Inheritance Used
+{
+    char line[800];
+    int N=1000;
+    int city_pos;
+
+public:
+    void choose_country_Buying_Land()
+    {
+        choose_country();
+    }
+
+    void choose_city(int flag)
+    {
+        ifstream fout;
+
+        if(flag==1)
+        {
+            fout.open("bangladesh_city_land.txt");
+
+            while (fout)
+            {
+                fout.getline(line, N);
+                cout << line << endl;
+            }
+            cout << "Choose City :: ";
+
+            cin >> city_pos;
+
+            Card_view_Adding card_view;
+            card_view.added_to_cart(city_pos,"bangladesh_city_land.txt");
+
+            fout.close();
+
+        }
+        else if(flag == 2)
+        {
+            fout.open("usa_city_land.txt");
+
+            while (fout)
+            {
+                fout.getline(line, N);
+                cout << line << endl;
+            }
+            cout << "Choose City :: ";
+
+            cin >> city_pos;
+
+            Card_view_Adding card_view;
+            card_view.added_to_cart(city_pos,"usa_city_land.txt");
+
+            fout.close();
+        }
+        else if(flag == 3)
+        {
+            fout.open("canada_city_land.txt");
+
+            while (fout)
+            {
+                fout.getline(line, N);
+                cout << line << endl;
+            }
+            cout << "Choose City :: ";
+
+            cin >> city_pos;
+
+            Card_view_Adding card_view;
+            card_view.added_to_cart(city_pos,"canada_city_land.txt");
+
+            fout.close();
+        }
+        else
+        {
+            cout << endl << "Wrong option is choosed !!" << endl;
+        }
+
+
+    }
+
+};
+
+class House_Rent : public DashBoard
+{
+    char line[600];
+    int N = 800;
+    int city_pos;
+public:
+    void choose_country_House_Rent()
+    {
+        choose_country();
+    }
+
+    void choose_city(int flag)
+    {
+        ifstream fout;
+
+        if(flag ==1 )
+        {
+
+            fout.open("bangladesh_city_rent.txt");
+
+            while (fout)
+            {
+                fout.getline(line, N);
+                cout << line << endl;
+            }
+            cout << "Choose your House :: ";
+
+            cin >> city_pos;
+
+            Card_view_Adding card_view;
+            card_view.added_to_cart(city_pos,"bangladesh_city_rent.txt");
+
+            fout.close();
+        }
+        else if(flag == 2)
+        {
+
+            fout.open("usa_city_rent.txt");
+
+            while (fout)
+            {
+                fout.getline(line, N);
+                cout << line << endl;
+            }
+            cout << "Choose City :: ";
+
+            cin >> city_pos;
+
+            Card_view_Adding card_view;
+            card_view.added_to_cart(city_pos,"usa_city_rent.txt");
+
+            fout.close();
+        }
+        else if(flag ==3)
+        {
+
+            fout.open("canada_city_rent.txt");
+
+            while (fout)
+            {
+                fout.getline(line, N);
+                cout << line << endl;
+            }
+            cout << "Choose City :: ";
+
+            cin >> city_pos;
+
+            Card_view_Adding card_view;
+            card_view.added_to_cart(city_pos,"canada_city_rent.txt");
+
+            fout.close();
+
+        }
+        else
+        {
+            cout << "Something went Wrong !!" << endl;
+        }
+
+    }
+
+
+
+};
+
+
+void calling_loging_class();
+
+class Login_sign_UP            // Class Used
+{
+    int count;
+    string user,pass,u,p;        // Encapsulation Used
+public:
+
+    void login()                  // Member Function Used
+    {
+
+        system("cls");
+        cout<<"Please enter the following details ::"<<endl;
+        cout<<"USERNAME : ";
+        cin>>user;
+        cout<<"PASSWORD :";
+        cin>>pass;
+
+        ifstream input("database.txt");
+        while(input>>u>>p)
+        {
+            if(u==user && p==pass)
+
+            {
+                count=1;
+                system("cls");
+            }
+        }
+        input.close();
+        if(count==1)
+        {
+            cout<<"**********Hello "<<user<<"*********\n*****LOGIN SUCESS*****\n***Thanks for logging in***\n";
+            Name_customer = user;
+
+            cout << endl << endl;
+            cout << "What do you want ?? "<< endl;
+            cout << "1. Do you want to buy land ?" << endl;
+            cout << "2. OR !! Do you want to rent a house ?" << endl;
+            cout << "Give your choice :: ";
+            int flag;
+            cin >> flag;
+
+            if(flag==1)
+            {
+                Buying_Land land;
+                land.choose_country_Buying_Land();
+            }
+            else if(flag == 2)
+            {
+                House_Rent land;
+                land.choose_country_House_Rent();
+            }
+            else
+            {
+                cout << "Wrong Input !!!!"<< endl << "Want to Login Again ? ==> ";
+                int k;
+                cin >> k;
+                if(k)
+                {
+                    login();
+                }
+                else
+                {
+                    exit(-1);
+                }
+
+
+            }
+        }
+        else
+        {
+            cout<<"\n LOGIN ERROR \n Please check your username and password \n ";
+
+            calling_loging_class();
+        }
+
+    }
+
+
+    void registr()
+    {
+
+        string reguser,regpass,ru,rp;
+        system("cls");
+        cout<<"Enter the username :";
+        cin>>reguser;
+        cout<<"\nEnter the password :";
+        cin>>regpass;
+
+        ofstream reg("database.txt",ios::app);
+        reg<<reguser<<' '<<regpass<<endl;
+        system("cls");
+        cout<<"\nRegistration Sucessful\n";
+
+        calling_loging_class();
+    }
+
+    void forgot()
+    {
+        int ch;
+        system("cls");
+        cout<<"Forgotten ? Don't worry !!\n";
+        cout<<"1.Search your id by username"<<endl;
+        cout<<"2.Search your id by password"<<endl;
+        cout<<"3.Main menu"<<endl;
+        cout<<"Enter your choice :";
+        cin>>ch;
+        switch(ch)
+        {
+        case 1:
+        {
+            int count=0;
+            string searchuser,su,sp;
+            cout<<"\nEnter your remembered username :";
+            cin>>searchuser;
+
+            ifstream searchu("database.txt");
+            while(searchu>>su>>sp)
+            {
+                if(su==searchuser)
+                {
+                    count=1;
+                }
+            }
+            searchu.close();
+            if(count==1)
+            {
+                cout<<"\n\Wow, account has been found\n";
+                cout<<"\nYour password is "<<sp;
+                cin.get();
+                cin.get();
+                system("cls");
+
+                calling_loging_class();
+            }
+            else
+            {
+                system("cls");
+                cout<<"\nSorry, Your userID is not found in our database\n";
+                cout<<"\nPlease kindly contact to Admin Of this App(Plaban Das -> 2007111)\n";
+                cin.get();
+                cin.get();
+
+                calling_loging_class();
+            }
+            break;
+        }
+        case 2:
+        {
+            int count=0;
+            string searchpass,su2,sp2;
+            cout<<"\nEnter the remembered password :";
+            cin>>searchpass;
+
+            ifstream searchp("database.txt");
+            while(searchp>>su2>>sp2)
+            {
+                if(sp2==searchpass)
+                {
+                    count=1;
+                }
+            }
+            searchp.close();
+            if(count==1)
+            {
+                cout<<"\nYour password is found in the database \n";
+                cout<<"\nYour Id is : "<<su2;
+                cin.get();
+                cin.get();
+                system("cls");
+
+                calling_loging_class();
+            }
+            else
+            {
+                system("cls");
+                cout<<"Sorry, We cannot found your password in our database \n";
+                cout<<"\nPlease kindly contact to Admin Of this App(Plaban Das -> 2007111)\n";
+                cin.get();
+                cin.get();
+
+                calling_loging_class();
+            }
+
+            break;
+        }
+
+        case 3:
+        {
+            cin.get();
+
+            calling_loging_class();
+        }
+        default:
+            cout<<"Sorry, You entered wrong choice. Kindly try again"<<endl;
+            forgot();
+        }
+    }
+
+};
+
+void calling_loging_class()
+{
+    Login_sign_UP obj;
+
+    int choice;
+    cout<<"***********************************************************************\n\n\n";
+    cout<<"                      Welcome to login page                               \n\n";
+    cout<<"*******************        MENU        *******************************\n\n";
+    cout<<"1.LOGIN"<<endl;
+    cout<<"2.REGISTER"<<endl;
+    cout<<"3.FORGOT PASSWORD (or) USERNAME"<<endl;
+    cout<<"4.Exit"<<endl;
+    cout<<"\nEnter your choice :";
+    cin>>choice;
+    cout<<endl;
+    switch(choice)
+    {
+    case 1:
+        obj.login();
+        break;
+    case 2:
+        obj.registr();
+        break;
+    case 3:
+        obj.forgot();
+        break;
+    case 4:
+
+        cout<<"\nThanks for using our App. Created By : Plaban Das -> 2007111(CSE-20)\n";
+        break;
+    default:
+        //system("cls");
+        cout<<"You've made a mistake , give it a try again\n"<<endl;
+        cout << "Want To Go Back ?? ::(1 for YES , 0 for NO) ::  ";
+    }
+
+}
+
+
+class Make_invoice
+{
+//    int product_id=0;
+//    char line[800];
+//    int N=1000;
+
+    int product_id;
+    char line[800];
+    int N;
+
+    char line1[800];
+public:
+    Make_invoice() {};
+    Make_invoice(int id,int n)
+    {
+        product_id = id;
+        N = n;
+    }
+
+
+    void make_invoice_ready(int flag)
+    {
+        ofstream out_invoice("invoice.txt");
+
+        int counter=0;
+        ifstream out;
+        out.open("card_view.txt");
+        while(out)
+        {
+            counter++;
+            if(counter == flag)
+            {
+                out.getline(line,N);
+
+
+
+                out_invoice << "*************************Welcome To HomeZone*************************" << endl;
+                out_invoice << "*******************Our Dream to make your life EASY******************" << endl << endl;
+                out_invoice << "***************Customer Name : "<< Name_customer << "***************" << endl;
+                out_invoice << "Product Details :: " << line << endl;
+
+
+                cout << endl << "Congratulations!! Your Invoice Is Ready...."<< endl;
+                cout << "Text Document has been generated !!" << endl << endl;
+                ifstream in_file;
+                in_file.open("invoice.txt");
+                while(in_file)
+                {
+                    in_file.getline(line1,N);
+                    cout << line1 << endl;
+                }
+
+
+                return;
+            }
+
+
+        }
+        out.close();
+        out_invoice.close();
+
+    }
+
+
+    void make_invoice()
+    {
+
+        ifstream out_card;
+        out_card.open("card_view.txt");
+        while(out_card)
+        {
+            product_id++;
+            out_card.getline(line,N);
+            cout << "Product ID: "<<product_id<< " Item :: "<< line << endl;
+        }
+        out_card.close();
+        cout << "Select Your Final Item **With Product ID** :: ";
+        int flag;
+        cin >> flag;
+        make_invoice_ready(flag);
+    }
+
+};
+
 
 int main()
 {
-   calling_loging_class();
+    calling_loging_class();
+
+    system("cls");
+
+    cout << "Do you want to watch final cart items ? (1 for YES , 0 for NO) :: " ;
+    int flag;
+    cin >> flag;
+    if(flag == 1)
+    {
+        Make_invoice invoice(0,1000);         //constructor used
+        invoice.make_invoice();
+    }
+    else
+    {
+        exit(-1);
+    }
 }
--- Land_Rent.cpp
+++ Land_Rent.cpp
@@ -5,7 +5,120 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "Land_Rent.cpp"
+
 using namespace std;
+
+class Card_view_Adding
+{
+    char line[600];
+    int N = 800;
+public:
+    void added_to_cart(int flag,string what_file)
+    {
+        int counter=0,c_ter=0;
+        ifstream fout;
+        fout.open(what_file);
+
+        if(flag >= 22)
+        {
+            counter = flag + 11;
+        }
+        else if(flag >=19)
+        {
+            counter = flag + 10;
+        }
+        else if(flag >=16)
+        {
+            counter = flag + 9;
+        }
+        else if(flag >=13)
+        {
+            counter = flag + 8;
+        }
+        else if(flag >=10)
+        {
+            counter = flag + 7;
+        }
+        else if(flag >=7)
+        {
+            counter = flag + 6;
+        }
+        else if(flag >=4)
+        {
+            counter = flag + 5;
+        }
+        else if(flag >=1)
+        {
+            counter = flag + 4;
+        }
+
+
+        while (fout)
+        {
+
+
+            c_ter++;
+            fout.getline(line, N);
+            if(c_ter==counter)
+            {
+                ofstream card_view("card_view.txt",ios::app);
+
+                card_view<<line<<endl;
+
+                cout << line << endl;
+                cout << endl << "Item is added to Cart Successfully" << endl;
+            }
+
+        }
+
+        cout << endl << "Do you want to stay more time ?? (1 for YES, 0 for NO)" <<endl;
+        cout << "Give input :: " ;
+        int a;
+        cin >> a;
+        if(a==1)
+        {
+
+            cout << endl << endl;
+            cout << "What do you want ?? "<< endl;
+            cout << "1. Do you want to buy land ?" << endl;
+            cout << "2. OR !! Do you want to rent a house ?" << endl;
+            cout << "Give your choice :: ";
+            int flag;
+            cin >> flag;
+
+            if(flag==1)
+            {
+                Buying_Land land;
+                land.choose_country_Buying_Land();
+            }
+            else if(flag == 2)
+            {
+                House_Rent land;
+                land.choose_country_House_Rent();
+            }
+            else
+            {
+                cout << "Wrong Input " << endl;
+            }
+
+
+
+        }
+        else if(a==0)
+        {
+            exit(-1);
+
+        }
+        else
+        {
+            cout << "Wrong Input " << endl;
+        }
+
+
+    }
+};
+
 
 class DashBoard
 {
@@ -13,6 +126,9 @@ class DashBoard
     char line[600];
     int N = 800;
 public:
+
+    virtual void choose_city(int flag) = 0; // Pure virtual function used
+
     void choose_country()
     {
         ifstream fin_country;
@@ -42,8 +158,6 @@ public:
         }
 
     }
-
-    virtual void choose_city(int flag) = 0; // Pure virtual function used
 };
 
 
@@ -63,10 +177,8 @@ public:
     {
         ifstream fout;
 
-        switch(flag)
+        if(flag==1)
         {
-        case 1:
-
             fout.open("bangladesh_city_land.txt");
 
             while (fout)
@@ -77,10 +189,15 @@ public:
             cout << "Choose City :: ";
 
             cin >> city_pos;
-            fout.close();
-            break;
-        case 2:
 
+            Card_view_Adding card_view;
+            card_view.added_to_cart(city_pos,"bangladesh_city_land.txt");
+
+            fout.close();
+
+        }
+        else if(flag == 2)
+        {
             fout.open("usa_city_land.txt");
 
             while (fout)
@@ -92,10 +209,13 @@ public:
 
             cin >> city_pos;
 
-            fout.close();
-            break;
-        case 3:
+            Card_view_Adding card_view;
+            card_view.added_to_cart(city_pos,"usa_city_land.txt");
 
+            fout.close();
+        }
+        else if(flag == 3)
+        {
             fout.open("canada_city_land.txt");
 
             while (fout)
@@ -106,9 +226,17 @@ public:
             cout << "Choose City :: ";
 
             cin >> city_pos;
+
+            Card_view_Adding card_view;
+            card_view.added_to_cart(city_pos,"canada_city_land.txt");
+
             fout.close();
-            break;
         }
+        else
+        {
+            cout << endl << "Wrong option is choosed !!" << endl;
+        }
+
 
     }
 
@@ -129,9 +257,8 @@ public:
     {
         ifstream fout;
 
-        switch(flag)
+        if(flag ==1 )
         {
-        case 1:
 
             fout.open("bangladesh_city_rent.txt");
 
@@ -140,12 +267,17 @@ public:
                 fout.getline(line, N);
                 cout << line << endl;
             }
-            cout << "Choose City :: ";
+            cout << "Choose your House :: ";
 
             cin >> city_pos;
+
+            Card_view_Adding card_view;
+            card_view.added_to_cart(city_pos,"bangladesh_city_rent.txt");
+
             fout.close();
-            break;
-        case 2:
+        }
+        else if(flag == 2)
+        {
 
             fout.open("usa_city_rent.txt");
 
@@ -158,9 +290,13 @@ public:
 
             cin >> city_pos;
 
+            Card_view_Adding card_view;
+            card_view.added_to_cart(city_pos,"usa_city_rent.txt");
+
             fout.close();
-            break;
-        case 3:
+        }
+        else if(flag ==3)
+        {
 
             fout.open("canada_city_rent.txt");
 
@@ -172,8 +308,12 @@ public:
             cout << "Choose City :: ";
 
             cin >> city_pos;
+
+            Card_view_Adding card_view;
+            card_view.added_to_cart(city_pos,"canada_city_rent.txt");
+
             fout.close();
-            break;
+
         }
 
     }
@@ -181,3 +321,4 @@ public:
 
 
 };
+
